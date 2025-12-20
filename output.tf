01b0888da1a80cd90ea65952090a0676879cb77d
opariffazman <ariff.azman@proton.me>
2025-12-20 12:55:32 +0800

    chore: removed deprecated output

--- output.tf
+++ output.tf
@@ -1,8 +1,4 @@
-output "ec2_public_ips" {
-  description = "Public IPs for EC2 nodes"
-  value       = [for inst in values(module.ec2_instance) : inst.public_ip]
-}
-
-output "public_key" {
-  value = tls_private_key.ssh_key.public_key_openssh
+output "ansible_inventory_file" {
+  description = "Path to generated Ansible inventory file"
+  value       = local_file.ansible_inventory.filename
 }
