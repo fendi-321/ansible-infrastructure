98d9c691d869c9dc27fbd6145825c08cb23b7d17
opariffazman <ariff.azman@proton.me>
2025-12-21 20:16:32 +0800

    chore: updated version for local provider & added tls version

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,11 @@ terraform {
     }
     local = {
       source  = "hashicorp/local"
-      version = "~> 2.0"
+      version = "2.6.1"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "4.1.0"
     }
   }
 }
